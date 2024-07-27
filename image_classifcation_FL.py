06edf4ed209f205e8df1b3b4e5d2529c3e2a6364
Brooke Joseph <114883803+brookejoseph@users.noreply.github.com>
2024-07-27 19:55:43 -0400

    Update image_classifcation_FL.py

--- image_classifcation_FL.py
+++ image_classifcation_FL.py
@@ -9,7 +9,6 @@ import time
 import random 
 import flwr as fl 
 
-#making sure we produce the same results everytime we run the same example 
 np.random.seed(0)
 from tensorflow.python.keras.optimizer_v2 import gradient_descent 
 from tensorflow_federated import python as tff 
@@ -21,17 +20,11 @@ BATCH_SIZE = 20
 SHUFFLE_BUFFER = 500 
 NUM_CLIENTS = 3
 
-#enabling the tensorflow version 2 behavior to make sure that this will all work 
 tf.compact.v1.enable_v2_behavior()
 
-#here we are just trying to simulate the types of datasets we would receive froma federated learning model 
-#tff already has some data sets that we can use
 emnist_train, emnist_test = tff.simulation.datasets.emnist.load_data()
 
 emnist_test.head()
-#here we are reshaping the data we recieved, and we are setting the pixels equal to x and labels equal to y 
-#reshaping the tensor to -1 simply means we want it to be 1 dimensional there for this example rather than having a 28x28 dataset we have a 784 element array
-#OrderedDict, simply is a way of remebereing the order in which the dat was collected 
 def preprocess(dataset):
     def element_fn(element):
         return collections.OrderedDict([
@@ -40,19 +33,13 @@ def preprocess(dataset):
         ])
     return dataset.repeat(NUM_EPOCHES).map(element_fn).shuffle(
         SHUFFLE_BUFFER).batch(BATCH_SIZE)
-
-#its important to note that a efficient way of feeding data to tff in a simulation is simply as a python list 
-#each element of the list should hold the data of an indivdual user
-# the data can also be entered with tf.data.Dataset 
 def make_federated_data(client_data, client_ids):
     return[preprocess(client_data.create_tf_dataset_for_client(x))
     for x in client_ids]
 
 sample_clients = emnist_train.client_ids[0: NUM_CLIENTS]
-#going through ID we said and it is going to donate the tensorflow dataset object
 
 
-#we are selcting 10 of our clients and we have prepocrssed the datsets in a way we picked 
 federated_train_data = make_federated_data(emnist_train, sample_clients)
 print(f'Number of clinet datasets:{len(federated_train_data)}')
 print(f'First dataset:{federated_train_data[0]}')
@@ -60,14 +47,12 @@ print(f'First dataset:{federated_train_data[0]}')
 sample_clients = emnist_test.client_ids[0: NUM_CLIENTS]
 federated_train_data = make_federated_data(emnist_test, sample_clients)
 
-#here we are providing a batch of our traning dataset, then we reconsturct an x and y 
 sample_batch = iter(federated_train_data[0]).next()
 sample_batch = collections.OrderedDict([
     ('x', sample_batch['x'].numpy()),
     ('y', sample_batch['y'].numpy()),
 ])
 
-#here we are just creating a Keras model needed
 def create_keras_model():
     return tf.keras.models.Sequntial([
         tf.keras.layers.Input.Layer(input_shapre=(784,)),
@@ -75,11 +60,9 @@ def create_keras_model():
         tf.keras.layers.Softmax(),
     ])
 
-#centrlized training 
 
 
 
-#here we are creating a new model we pass in the model, then an input sepc to give tff an example of the data they're consuming 
 def model_fn():
     keras_model = create_keras_model()
     return tff.learning.from_keras_model(
